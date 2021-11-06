04c511c55af5ae0f283386857a599c0c0406e157
spatterss135 <spatterss135@gmail.com>
2021-11-06 09:18:14 -0500

    Refactored Code/Added rest/spread operators

--- public/ingredientList.js
+++ public/ingredientList.js
@@ -23,16 +23,17 @@ const ingredientList = {
         ingredientCard.className = 'card'
 
         const sandwichHasIngredient = cart.selectedSandwich.ingredients.includes(ingredient.name);
+        const {name, calories, imageURL, description} = ingredient
         ingredientCard.innerHTML = `
         <div class="card-body">
             <div class="row g-0">
                 <div class="col-sm-4">
-                    <img src="${ingredient.imageURL}" alt="${ingredient.name}" style="width: 100%">
+                    <img src="${imageURL}" alt="${name}" style="width: 100%">
                 </div>
                 <div class="col-sm-8">
-                    <h5 class="card-title">${ingredient.name}</h5>
-                    <h6 class="card-subtitle mb-2 text-muted">${ingredient.calories} Calories</h6>
-                    <p class="card-text">${ingredient.description}</p>
+                    <h5 class="card-title">${name}</h5>
+                    <h6 class="card-subtitle mb-2 text-muted">${calories} Calories</h6>
+                    <p class="card-text">${description}</p>
                     ${sandwichHasIngredient ? `
                         <button class="btn btn-danger toggle-button">Remove</button>
                     ` : `
